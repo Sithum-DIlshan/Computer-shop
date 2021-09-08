2516cb97386de84f24856dd484fe35ba6d1249d7
Sithum-DIlshan <79800028+Sithum-DIlshan@users.noreply.github.com>
2021-09-08 17:30:26 +0530

    Add files via upload

---
+++ Computer Shop/src/AppInitializer.java
@@ -0,0 +1,46 @@
+import javafx.animation.TranslateTransition;
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.paint.Color;
+import javafx.stage.Stage;
+import javafx.stage.StageStyle;
+import javafx.util.Duration;
+
+import java.io.IOException;
+
+public class AppInitializer extends Application {
+    private double x, y;
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) throws IOException {
+        Parent root = FXMLLoader.load(getClass().getResource("view/LoginForm.fxml"));
+        Scene scene = new Scene(root);
+        scene.setFill(Color.TRANSPARENT);
+        primaryStage.setScene(scene);
+        primaryStage.initStyle(StageStyle.TRANSPARENT);
+
+        root.setOnMousePressed(event -> {
+            x = event.getSceneX();
+            y = event.getSceneY();
+        });
+        root.setOnMouseDragged(event -> {
+
+            primaryStage.setX(event.getScreenX() - x);
+            primaryStage.setY(event.getScreenY() - y);
+
+        });/*
+        TranslateTransition translate = new TranslateTransition();
+        translate.setDuration(Duration.seconds(0.7));
+        translate.setNode(root);
+        translate.setToX(49);
+        translate.play();*/
+
+        primaryStage.show();
+    }
+}
---
+++ Computer Shop/src/controller/LoginFormController.java
@@ -0,0 +1,10 @@
+package controller;
+
+import javafx.animation.TranslateTransition;
+import javafx.event.ActionEvent;
+
+public class LoginFormController {
+    public void exit(ActionEvent actionEvent) {
+        System.exit(1);
+    }
+}
