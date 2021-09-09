866b324573bb20164e836e12e3025c2549512ca4
Sithum-DIlshan <sithumdilshan985@gmail.com>
2021-09-09 15:41:20 +0530

    Edited

--- Computer Shop/src/controller/LoginFormController.java
+++ Computer Shop/src/controller/LoginFormController.java
@@ -1,10 +1,38 @@
 package controller;
 
+import com.jfoenix.controls.JFXPasswordField;
+import com.jfoenix.controls.JFXTextField;
 import javafx.animation.TranslateTransition;
 import javafx.event.ActionEvent;
+import javafx.fxml.Initializable;
+import javafx.scene.input.KeyEvent;
 
-public class LoginFormController {
+import java.net.URL;
+import java.util.LinkedHashMap;
+import java.util.ResourceBundle;
+import java.util.regex.Pattern;
+
+public class LoginFormController implements Initializable {
+    public JFXTextField txtUsrName;
+    public JFXPasswordField txtPassword;
+    LinkedHashMap<JFXTextField, Pattern> txtFieldValidate = new LinkedHashMap();
+    LinkedHashMap<JFXPasswordField, Pattern> passwordFieldValidate = new LinkedHashMap();
+
     public void exit(ActionEvent actionEvent) {
         System.exit(1);
     }
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+        validate_init();
+    }
+
+    public void validate_data(KeyEvent keyEvent) {
+
+    }
+
+    private void validate_init() {
+
+    }
+
 }
