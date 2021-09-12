e6acfa8325e47ef84463294ea9b1e53914096dd9
Sithum-DIlshan <sithumdilshan985@gmail.com>
2021-09-12 15:05:50 +0530

    Edited

--- Computer Shop/src/controller/TestController.java
+++ Computer Shop/src/controller/TestController.java
@@ -4,6 +4,8 @@ import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
 import javafx.scene.Node;
 import javafx.scene.control.ScrollPane;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.Pane;
 import javafx.scene.layout.VBox;
 
 import java.io.IOException;
@@ -14,21 +16,23 @@ public class TestController implements Initializable {
 
     public VBox getItems;
     public ScrollPane scroll;
+    public Pane pane;
+    public BorderPane border;
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
        Node[] nodes = new Node[10];
-        for (int i = 0; i < nodes.length; i++){
+  /*     for (int i = 0; i < nodes.length; i++){
             try {
                 nodes[i] = FXMLLoader.load(getClass().getResource("/view/OverviewNode.fxml"));
                 getItems.getChildren().addAll(nodes[i]);
             } catch (IOException e) {
                 e.printStackTrace();
             }
-        }
+        }*/
         /*try {
-            nodes[0] = (Node) FXMLLoader.load(getClass().getResource("view/OverviewNode.fxml"));
-            getItems.getChildren().add((javafx.scene.Node) nodes[0]);
+            nodes[0] = FXMLLoader.load(getClass().getResource("/view/SystemReports.fxml"));
+            getItems.getChildren().add(nodes[0]);
         } catch (IOException e) {
             e.printStackTrace();
         }*/
@@ -38,5 +42,11 @@ public class TestController implements Initializable {
         } catch (IOException e) {
             e.printStackTrace();
         }*/
+        try {
+            nodes[0] = FXMLLoader.load(getClass().getResource("/view/SystemReports.fxml"));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        border.setCenter(nodes[0]);
     }
 }
